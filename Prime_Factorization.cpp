6ac7ac8c86e90be22600e095b33c0d13b7c0ebfc
mohitmp9107 <43476438+mohitmp9107@users.noreply.github.com>
2019-09-04 20:37:07 +0530

    Add files via upload

---
+++ Prime_Factorization.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int primefactorization(int a ){
+
+    while(a%2==0){
+        cout << 2 << endl;
+        a/=2;
+    }
+    // cout << a << endl;
+    for(int i=3;i<=sqrt(a);i+=2){
+        while(a%i==0){
+            cout << i << endl;
+            a/=i;
+        }
+
+    }
+    if(a>2) cout << a <<endl;
+}
+int main()
+{
+    int a;   cin >> a;
+    primefactorization(a) ;
+}
